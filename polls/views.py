5510394cac551762f4c305217ebb401003d6764f
duraznoj <sashka.warner@gmail.com>
2022-09-22 00:35:11 -0700

    Add form processing and streamline views

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
+"""
+#"hard way"
 from curses.ascii import HT
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 #from django.template import loader
 #from django.http import Http404
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -34,6 +37,78 @@ def results(request, question_id):
   return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question=get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST["choice"])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, "polls/detail.html", {
+      "question": question,
+      "error_message": "You didn't select a choice",
+    })
+  else:
+    selected_choice.votes += 1 
+    #can change above increment statement to 
+    # selected_choice.votes = F('votes') + 1 to avoid race condition
+    # race condition in this case would be two users voting at exactly the same time
+    # F() has the DB update the value instead of python, which will save both users
+    # simultaneous update, whereas with python if there are two simultaneous threads
+    # one will succeed and one will fail if they are executed at the same time
+    selected_choice.save()
+     # Always return an HttpResponseRedirect after successfully dealing
+      # with POST data. This prevents data from being posted twice if a
+      # user hits the Back button.
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+    #reverse just gets what is defined in polls/urls.py for path(name="results")
+    #which in this case is "<int:question_id>/results/"
+    
+  #return HttpResponse("You're voting on question %s." % question_id)
 
 
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, "polls/results.html", {"question": question})
+"""
+
+#"generic views" way
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+  template_name = "polls/index.html"
+  context_object_name = "latest_question_list"
+
+  def get_queryset(self):
+    #return last 5 published questions
+    return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = "polls/results.html"
+
+def vote(request, question_id):
+  #note: same as "hard way" above
+  question=get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST["choice"])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, "polls/detail.html", {
+      "question": question,
+      "error_message": "You didn't select a choice",
+    })
+  else:
+    selected_choice.votes += 1 
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+  
